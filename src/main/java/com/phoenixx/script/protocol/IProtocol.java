1a3098aa744ccea1717a9f0597bdcf7c9e791782
Phoenix <goldenphoenix201@gmail.com>
2023-04-28 19:21:27 -0400

    Added protocol interface

---
+++ src/main/java/com/phoenixx/script/protocol/IProtocol.java
@@ -0,0 +1,15 @@
+package com.phoenixx.script.protocol;
+
+/**
+ * @author Junaid Talpur
+ * @project Hydra
+ * @since 7:17 PM [28-04-2023]
+ */
+public interface IProtocol {
+
+    /**
+     * Get the name of the protocol.
+     * @return String.
+     */
+    String getName();
+}
