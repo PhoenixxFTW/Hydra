f78612c3c1ce968baebb9445765d82dde940af54
Phoenix <goldenphoenix201@gmail.com>
2023-05-16 11:37:27 -0400

    Got a example of the levenshtein algorithm working. This is probably the implementation Im going to stick with till I find something better

--- src/main/java/com/phoenixx/core/snapshots/data/DataObj.java
+++ src/main/java/com/phoenixx/core/snapshots/data/DataObj.java
@@ -1,5 +1,10 @@
 package com.phoenixx.core.snapshots.data;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,5 +16,27 @@ import java.util.Map;
 public class DataObj {
     public String fullBody;
     // FIXME Will not support key / value pair with a data object as the pair
-    public Map<String, String> keyPairData = new HashMap<>();
+    public Map<String, Object> keyPairData = new HashMap<>();
+
+    public JsonObject jsonObject;
+
+    public DataObj(String fullBody) {
+        this.setBody(fullBody);
+    }
+
+    public DataObj() {
+        super();
+    }
+
+    public void setBody(String fullBody) {
+        this.fullBody = fullBody;
+        this.convertToMap();
+    }
+
+    public void convertToMap() {
+        Type mapType = new TypeToken<Map<String, Map>>(){}.getType();
+        Gson gson = new Gson();
+        keyPairData = gson.fromJson(this.fullBody, new TypeToken<HashMap<String, Object>>() {}.getType());
+        jsonObject = gson.fromJson(this.fullBody, JsonObject.class);
+    }
 }
--- src/test/java/LevenshteinAlgorithmTest.java
+++ src/test/java/LevenshteinAlgorithmTest.java
@@ -1,13 +1,11 @@
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.phoenixx.core.snapshots.HTTPObject;
-import com.phoenixx.core.snapshots.data.TreeNode;
-import com.phoenixx.core.snapshots.impl.Snapshot;
+import com.google.gson.JsonElement;
+import com.phoenixx.core.snapshots.data.DataObj;
 import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Junaid Talpur
@@ -18,18 +16,10 @@ public class LevenshteinAlgorithmTest {
     private static final double SIMILARITY_THRESHOLD = 0.97; // Adjust the threshold as per your needs
 
     public static void main(String[] args) {
-        List<TreeNode> treeNodes = new ArrayList<>();
-        List<HTTPObject> requests = new ArrayList<>();
+        List<DataObj> requests = new ArrayList<>();
 
-        /*for(int i = 0; i < 5; i++) {
-            HTTPObject httpObject = new HTTPObject("test", "test");
-            httpObject.setBody("{\"type\":\"DF_ADEOS\",\"id\":\""+i+"-14DK29M\",\"status\":\"In Progress\",\"contactId\":\"1-14D9RX8\",\"documentAdminId\":\"1-14CZNDD\",\"caseId\":\"1-14D9RWW\",\"formLastUpdatedTS\":\"04/05/2023 13:39:52\",\"signedDate\":\"\",\"signedBy\":\"\",\"signedFlag\":false,\"formData\":{\"id\":\"1-14DK29N\",\"payorSection\":{\"emailAddress\":\"JhonDoe@ontario.ca\",\"addressType\":\"Canada\",\"canadaUsaAddress\":{\"unitNumber\":\"\",\"streetNumber\":\"123\",\"streetName\":\"Drury\",\"poBox\":\"\",\"city\":\"Toronto\",\"country\":\"Canada\",\"province\":\"ON\",\"addressLine1\":null,\"addressLine2\":null,\"postalCode\":\"A1A 1A1\",\"addressInfo\":null,\"town\":\"\",\"state\":null,\"zipCode\":null},\"internationalAddress\":null,\"phoneNumType\":\"USA\",\"phoneNumber\":{\"homePhone\":\"(905) 428-5395\",\"cellPhone\":\"(123) 465-7685\",\"businessPhone\":\"\"},\"payorDtls\":{\"firstName\":\"John\",\"lastName\":\"Doe\"}},\"recipientSection\":{\"emailAddress\":\"\",\"addressType\":\"\",\"canadaUsaAddress\":null,\"internationalAddress\":null,\"phoneNumType\":\"\",\"phoneNumber\":{\"homePhone\":\"\",\"cellPhone\":\"\",\"businessPhone\":\"\"},\"recipientDtls\":{\"firstName\":\"\",\"lastName\":\"\"}},\"typeOfSupportList\":{\"supportOrderTypeSpouseFlag\":false,\"supportOrderTypeChildFlag\":false},\"spousalDtls\":null,\"childDtlsArray\":null},\"formType\":\"DF_ADEOS\",\"serviceRequestNum\":null}");
-            JsonObject jsonObject = JsonParser.parseString(httpObject.getBody()).getAsJsonObject();
 
-            treeNodes.add(Snapshot.constructTree(jsonObject));
-        }*/
-
-        HTTPObject httpObject = new HTTPObject("test", "test");
+        DataObj httpObject = new DataObj();
         httpObject.setBody("{\n" +
                 "    \"_id\": \"6463969d293ec28993deac4c\",\n" +
                 "    \"index\": 0,\n" +
@@ -46,11 +36,9 @@ public class LevenshteinAlgorithmTest {
                 "    \"phone\": \"+1 (804) 520-2682\"\n" +
                 "  }");
 
-        JsonObject jsonObject = JsonParser.parseString(httpObject.getBody()).getAsJsonObject();
-        treeNodes.add(Snapshot.constructTree(jsonObject));
         requests.add(httpObject);
 
-        httpObject = new HTTPObject("test", "test");
+        httpObject = new DataObj();
         httpObject.setBody("{\n" +
                 "    \"_id\": \"6463969ddc0245c90c6e0513\",\n" +
                 "    \"index\": 1,\n" +
@@ -66,13 +54,11 @@ public class LevenshteinAlgorithmTest {
                 "    \"email\": \"freidabriggs@magnina.com\",\n" +
                 "    \"phone\": \"+1 (820) 590-3556\"\n" +
                 "  }");
-        jsonObject = JsonParser.parseString(httpObject.getBody()).getAsJsonObject();
-        treeNodes.add(Snapshot.constructTree(jsonObject));
         requests.add(httpObject);
 
-        httpObject = new HTTPObject("test", "test");
+        httpObject = new DataObj();
         httpObject.setBody("{\n" +
-                "    \"_id\": \"6463969d293ec28993deac4c\",\n" +
+                "    \"_id\": \"6463969da850d5678c0d66c4\",\n" +
                 "    \"index\": 2,\n" +
                 "    \"guid\": \"8aceaf88-5d88-49bd-a01e-be7157831f1c\",\n" +
                 "    \"isActive\": false,\n" +
@@ -86,44 +72,67 @@ public class LevenshteinAlgorithmTest {
                 "    \"email\": \"yeseniabernard@illumity.com\",\n" +
                 "    \"phone\": \"+1 (920) 559-2800\"\n" +
                 "  }");
-        jsonObject = JsonParser.parseString(httpObject.getBody()).getAsJsonObject();
-        treeNodes.add(Snapshot.constructTree(jsonObject));
         requests.add(httpObject);
 
         // The lower this threshold is, the more precise value the algorithm will look for.
         int threshold = 3; // Maximum acceptable Levenshtein distance
         LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+
         for(int i = 0; i < requests.size(); i++) {
-            HTTPObject firstObj = requests.get(i);
+            DataObj firstObj = requests.get(i);
+
+            System.out.println("\n------------------ STARTING WITH #"+i +" ------------------");
+            for(int j = i + 1; j < requests.size(); j++) {
+                DataObj secondObj = requests.get(j);
+                System.out.println("COMPARING TO OBJ: #" + j);
+
+                for (Map.Entry<String, JsonElement> entry1 : firstObj.jsonObject.entrySet()) {
+                    for (Map.Entry<String, JsonElement> entry2 : secondObj.jsonObject.entrySet()) {
+                        if (entry1.getKey().equals(entry2.getKey())) {
+                            if (entry1.getValue().isJsonPrimitive() && entry2.getValue().isJsonPrimitive()) {
+                                String value1 = entry1.getValue().getAsString();
+                                String value2 = entry2.getValue().getAsString();
+
+                                int distance = levenshteinDistance.apply(value1, value2);
+                                System.out.println("Key: " + entry1.getKey() + ", Levenshtein Distance: " + distance + " val1: " + value1 + " val2: " + value2);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /*for(int i = 0; i < requests.size(); i++) {
+            DataObj firstObj = requests.get(i);
 
             for(int j = 1; j < requests.size() - 1; j++) {
-                HTTPObject secondObj = requests.get(j);
+                DataObj secondObj = requests.get(j);
 
                 if(i == j) {
                     continue;
                 }
 
-                int distance = levenshteinDistance.apply(firstObj.getBody(), secondObj.getBody());
-                System.out.println("Distance: " + distance +  " between: " + i +  " and val: " + j);
-                if (distance <= threshold) {
-                    System.out.println("Potential Correlation Candidate: " +
-                            "\n ->" + firstObj.getBody() + "" +
-                            "\n ->" + secondObj.getBody());
+                // Loop through all data in the first obj
+                for(String[] valArray: firstObj.keyPairData.values()) {
+                    for(String val1: valArray) {
+                        System.out.println("VAL1: " + val1);
+                        // Now here we loop through the second objects valArray and data in it
+                        for(String[] valArray2: secondObj.keyPairData.values()) {
+                            for(String val2: valArray2) {
+                                // Now here we loop through the second objects valArray and data in it
+
+                                int distance = levenshteinDistance.apply(val1, val2);
+                                System.out.println("Distance: " + distance +  " between: " + val1 +  " and val: " + val2);
+                                if (distance <= threshold) {
+                                    System.out.println("Potential Correlation Candidate: " +
+                                            "\n ->" + val1 + "" +
+                                            "\n ->" + val2);
+                                }
+                            }
+                        }
+
+                    }
                 }
-            }
-        }
-
-       /* // Loop through all tree's
-        for(int i = 0; i < treeNodes.size(); i++) {
-            TreeNode tree1 = treeNodes.get(i);
-
-            // Loop through all trees starting from the 2nd element (1st index)
-            for(int j = 1; j < treeNodes.size() - 1; j++) {
-                TreeNode tree2 = treeNodes.get(j);
-
-                LevenshteinDistance distance = new LevenshteinDistance();
-                int similarityScore = Snapshot.calculateSimilarity(node1, node2, distance);
-                System.out.println("Similarity score: " + similarityScore);
             }
         }*/
     }
